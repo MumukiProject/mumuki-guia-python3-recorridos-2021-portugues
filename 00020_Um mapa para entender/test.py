d7653129d431b601181f65a7b05ee720fa06520b
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 21:41:16 +0000

    Mumuki Export on 2022-08-25 21:41:16 +0000

--- 00020_Um mapa para entender/test.py
+++ 00020_Um mapa para entender/test.py
@@ -1,6 +1,6 @@
 
   def test_meses_de_un_trimestre_devuelve_tres_meses(self):
-    self.assertEqual(meses([{ "mes": "janeiro", "ganancia": 10 }, { "mes": "fevereiro", "ganancia": 2 }, { "mes": "março", "ganancia": 5 }]), ["janeiro", "fevereiro", "março"])
+    self.assertEqual(meses([{ "mes": "janeiro", "lucro": 10 }, { "mes": "fevereiro", "lucro": 2 }, { "mes": "março", "lucro": 5 }]), ["janeiro", "fevereiro", "março"])
   
   def test_meses_de_un_semestre_devuelve_seis_meses(self):
-    self.assertEqual(meses([{ "mes": "janeiro", "ganancia": 10 }, { "mes": "fevereiro", "ganancia": 2 }, { "mes": "março", "ganancia": 5 }, { "mes": "abril", "ganancia": 8 }, { "mes": "maio", "ganancia": 12 }, { "mes": "junho", "ganancia": 25 }]), ["janeiro", "fevereiro", "março", "abril", "maio", "junho"])
+    self.assertEqual(meses([{ "mes": "janeiro", "lucro": 10 }, { "mes": "fevereiro", "lucro": 2 }, { "mes": "março", "lucro": 5 }, { "mes": "abril", "lucro": 8 }, { "mes": "maio", "lucro": 12 }, { "mes": "junho", "lucro": 25 }]), ["janeiro", "fevereiro", "março", "abril", "maio", "junho"])
