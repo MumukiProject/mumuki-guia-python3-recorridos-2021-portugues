95d8c05a76f8d6a82dbb1b33f3257ed8dc274dc1
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 22:44:09 +0000

    Mumuki Export on 2022-08-25 22:44:09 +0000

--- 00017_Todo o lucro, o lucro/test.py
+++ 00017_Todo o lucro, o lucro/test.py
@@ -1,6 +1,6 @@
   
   def test_ganancia_total_de_balances_del_primer_trimestre_es_menos_8(self):
-    self.assertEqual(ganancia_total([{ "mes": "janeiro", "lucro": 2 }, { "mes": "fevereiro", "lucro": 10 }, { "mes": "março", "lucro": -20 }]), -8)
+    self.assertEqual(lucro_total([{ "mes": "janeiro", "lucro": 2 }, { "mes": "fevereiro", "lucro": 10 }, { "mes": "março", "lucro": -20 }]), -8)
   
   def test_ganancia_total_de_balances_del_ultimo_semestre_es_1538(self):
-    self.assertEqual(ganancia_total([{ "mes": "julho", "lucro": 50 }, { "mes": "agosto", "lucro": -12 }, { "mes": "setembro", "lucro": 1000 }, { "mes": "outubro", "lucro": 300 }, { "mes":  "novembro", "lucro": 200 }, { "mes": "dezembro", "lucro": 0 }]), 1538)
+    self.assertEqual(lucro_total([{ "mes": "julho", "lucro": 50 }, { "mes": "agosto", "lucro": -12 }, { "mes": "setembro", "lucro": 1000 }, { "mes": "outubro", "lucro": 300 }, { "mes":  "novembro", "lucro": 200 }, { "mes": "dezembro", "lucro": 0 }]), 1538)
