e113406dbadb4ef4fc6bde6b378f58e55e5dd67c
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 22:06:24 +0000

    Mumuki Export on 2022-08-25 22:06:23 +0000

--- 00009_Filtrar, filtrar cada coisa no seu lugar/test.py
+++ 00009_Filtrar, filtrar cada coisa no seu lugar/test.py
@@ -1,12 +1,12 @@
 
   def test_balances_positivos_devuelve_todos_los_balances_si_todos_tienen_ganancia_mayor_a_cero(self):
-	  self.assertEqual(balanco_positivo([{ "mes": "março", "ganancia": 10 }, { "mes": "agosto", "ganancia": 2 }, { "mes": "setembro", "ganancia": 8 }]), [{ "mes": "março", "ganancia": 10 }, { "mes": "agosto", "ganancia": 2 }, { "mes": "setembro", "ganancia": 8 }])
+	  self.assertEqual(balanco_positivo([{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro": 8 }]), [{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro": 8 }])
 
   def test_balances_positivos_excluye_a_los_balances_con_ganancia_cero(self):
-	  self.assertEqual(balanco_positivo([{ "mes": "março", "ganancia": 10 }, { "mes": "agosto", "ganancia": 2 }, { "mes": "setembro", "ganancia": 0 }]), [{ "mes": "março", "ganancia": 10 }, { "mes": "agosto", "ganancia": 2 }])
+	  self.assertEqual(balanco_positivo([{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro": 0 }]), [{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }])
 
   def test_balances_positivos_excluye_a_los_balances_con_ganancia_negativa(self):
-	  self.assertEqual(balanco_positivo([{ "mes": "julho", "ganancia": 12 }, { "mes": "agosto", "ganancia": -2 }]), [{ "mes": "julho", "ganancia": 12 }])
+	  self.assertEqual(balanco_positivo([{ "mes": "julho", "lucro": 12 }, { "mes": "agosto", "lucro": -2 }]), [{ "mes": "julho", "lucro": 12 }])
  
   def test_balances_positivos_devuelve_la_lista_vacia_si_todos_los_balances_tienen_ganancia_de_cero_o_menos(self):
-	  self.assertEqual(balanco_positivo([{ "mes": "agosto", "ganancia": -12 }, { "mes": "setembro", "ganancia": 0 }]), [])
+	  self.assertEqual(balanco_positivo([{ "mes": "agosto", "lucro": -12 }, { "mes": "setembro", "lucro": 0 }]), [])
