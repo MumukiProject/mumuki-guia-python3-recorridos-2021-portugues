d6335dc85356c54ca4236f6012c6c7c10a32f33c
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 21:42:12 +0000

    Mumuki Export on 2022-08-25 21:42:12 +0000

--- 00008_Sou o mapa, sou o mapa/test.py
+++ 00008_Sou o mapa, sou o mapa/test.py
@@ -1,9 +1,9 @@
 
   def test_ganancias_de_los_balances_de_un_periodo_de_tres_meses_me_devuelve_solo_los_valores_de_las_ganancias_de_esos_tres_meses(self):
-	  self.assertEqual(lucros([{ "mes": "março", "ganancia": 10 }, { "mes": "agosto", "ganancia": 2 }, { "mes": "setembro", "ganancia":0 }]), [10, 2, 0])
+	  self.assertEqual(lucros([{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro":0 }]), [10, 2, 0])
 
   def test_ganancias_de_los_balances_de_un_periodo_de_cuatro_meses_me_devuelve_solo_los_valores_de_las_ganancias_de_esos_cuatro_meses(self):
-	  self.assertEqual(lucros([{ "mes": "março", "ganancia": 10 }, { "mes": "agosto", "ganancia": 2 }, { "mes": "setembro", "ganancia":0 }, { "mes": "dezembro", "ganancia": 8 }]), [10, 2, 0, 8])
+	  self.assertEqual(lucros([{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro":0 }, { "mes": "dezembro", "lucro": 8 }]), [10, 2, 0, 8])
 
   def test_ganancias_de_los_balances_de_un_periodo_de_dos_meses_me_devuelve_solo_los_valores_de_las_ganancias_de_esos_dos_meses(self):
-	  self.assertEqual(lucros([{ "mes": "março", "ganancia": 8 }, { "mes": "agosto", "ganancia": 7 }]), [8,7])
+	  self.assertEqual(lucros([{ "mes": "março", "lucro": 8 }, { "mes": "agosto", "lucro": 7 }]), [8,7])
