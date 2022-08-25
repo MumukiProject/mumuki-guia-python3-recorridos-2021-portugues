b66a1034ce1a38953afbf8c5d58996ef98d92087
rocio@mumuki.org <rocio@mumuki.org>
2022-08-25 22:30:36 +0000

    Mumuki Export on 2022-08-25 22:30:36 +0000

--- 00023_Longo este mês/test.py
+++ 00023_Longo este mês/test.py
@@ -1,12 +1,12 @@
 
   def test_los_balances_de_meses_largos_del_primer_semestre_retorna_los_balances_de_enero_marzo_y_mayo(self):
-  	self.assertEqual(balances_de_meses_largos([{ "mes": "janeiro", "lucro": 1000 }, { "mes": "fevereiro", "lucro": -200 }, { "mes": "março", "lucro": 500 }, { "mes": "abril", "lucro": 800 }, { "mes": "maio", "lucro": 770 }, { "mes": "junho", "lucro": 870 }]), [{ "mes": "janeiro", "lucro": 1000 }, { "mes": "março", "lucro": 500 }, { "mes": "maio", "lucro": 770 }])
+  	self.assertEqual(balancos_de_meses_longos([{ "mes": "janeiro", "lucro": 1000 }, { "mes": "fevereiro", "lucro": -200 }, { "mes": "março", "lucro": 500 }, { "mes": "abril", "lucro": 800 }, { "mes": "maio", "lucro": 770 }, { "mes": "junho", "lucro": 870 }]), [{ "mes": "janeiro", "lucro": 1000 }, { "mes": "março", "lucro": 500 }, { "mes": "maio", "lucro": 770 }])
 
   def test_los_balances_de_meses_largos_del_ultimo_semestre_retorna_los_balances_de_julio_agosto_octubre_y_diciembre(self):
-  	self.assertEqual(balances_de_meses_largos([{ "mes": "julho", "lucro": 500 }, { "mes": "agosto", "lucro": 900 }, { "mes": "setembro", "lucro": 1800 }, { "mes": "outubro", "lucro": 900 }, { "mes": "novembro", "lucro": 2300 }, { "mes": "dezembro", "lucro": 2000 }]), [{ "mes": "julho", "lucro": 500 }, { "mes": "agosto", "lucro": 900 }, { "mes": "outubro", "lucro": 900 }, { "mes": "dezembro", "lucro": 2000 }])
+  	self.assertEqual(balancos_de_meses_longos([{ "mes": "julho", "lucro": 500 }, { "mes": "agosto", "lucro": 900 }, { "mes": "setembro", "lucro": 1800 }, { "mes": "outubro", "lucro": 900 }, { "mes": "novembro", "lucro": 2300 }, { "mes": "dezembro", "lucro": 2000 }]), [{ "mes": "julho", "lucro": 500 }, { "mes": "agosto", "lucro": 900 }, { "mes": "outubro", "lucro": 900 }, { "mes": "dezembro", "lucro": 2000 }])
 
   def test_los_balances_de_meses_largos_del_primer_trimestre_retornan_los_balances_de_enero_y_marzo(self):
-  	self.assertEqual(balances_de_meses_largos([{ "mes": "janeiro", "lucro": 1000 }, { "mes": "fevereiro", "lucro": -200 }, { "mes": "março", "lucro": 500 }]), [{ "mes": "janeiro", "lucro": 1000 }, { "mes": "março", "lucro": 500 }])
+  	self.assertEqual(balancos_de_meses_longos([{ "mes": "janeiro", "lucro": 1000 }, { "mes": "fevereiro", "lucro": -200 }, { "mes": "março", "lucro": 500 }]), [{ "mes": "janeiro", "lucro": 1000 }, { "mes": "março", "lucro": 500 }])
 
   def test_los_balances_de_meses_largos_del_segundo_trimestre_retorna_el_balance_de_mayo(self):
-  	self.assertEqual(balances_de_meses_largos([{ "mes": "abril", "lucro": 800 }, { "mes": "maio", "lucro": 770 }, { "mes": "junho", "lucro": 870 }]), [{ "mes": "maio", "lucro": 770 }])
+  	self.assertEqual(balancos_de_meses_longos([{ "mes": "abril", "lucro": 800 }, { "mes": "maio", "lucro": 770 }, { "mes": "junho", "lucro": 870 }]), [{ "mes": "maio", "lucro": 770 }])
