9b71f990c682039e244b5b85f629c28dcdd1982e
rocio@mumuki.org <rocio@mumuki.org>
2022-09-27 23:28:22 +0000

    Mumuki Export on 2022-09-27 23:28:22 +0000

--- 00013_Como os meses passam!/test.py
+++ 00013_Como os meses passam!/test.py
@@ -1,6 +1,6 @@
 
-  def test_meses_de_un_trimestre_devuelve_tres_meses(self):
+  def test_meses_de_um_trimestre_retorna_três_meses(self):
     self.assertEqual(meses([{ "mes": "janeiro", "lucro": 10 }, { "mes": "fevereiro", "lucro": 2 }, { "mes": "março", "lucro": 5 }]), ["janeiro", "fevereiro", "março"])
   
-  def test_meses_de_un_semestre_devuelve_seis_meses(self):
+  def test_meses_de_um_semestre_retorna_seis_meses(self):
     self.assertEqual(meses([{ "mes": "janeiro", "lucro": 10 }, { "mes": "fevereiro", "lucro": 2 }, { "mes": "março", "lucro": 5 }, { "mes": "abril", "lucro": 8 }, { "mes": "maio", "lucro": 12 }, { "mes": "junho", "lucro": 25 }]), ["janeiro", "fevereiro", "março", "abril", "maio", "junho"])
