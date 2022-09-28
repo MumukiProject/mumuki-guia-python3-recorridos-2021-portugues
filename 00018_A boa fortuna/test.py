ffef4d65bc768540149b6bbe73b1faafe792db32
rocio@mumuki.org <rocio@mumuki.org>
2022-09-28 00:09:07 +0000

    Mumuki Export on 2022-09-28 00:09:07 +0000

--- 00018_A boa fortuna/test.py
+++ 00018_A boa fortuna/test.py
@@ -1,6 +1,6 @@
 
-  def test_afortunados_devuelve_los_balances_cuya_ganancia_es_mayor_a_1000(self):
+  def test_afortunados_retorna_os_balanços_cujo_ganho_é_maior_que_1000(self):
     self.assertEqual(sortudo([{ "mes": "janeiro", "lucro": 1000 }, { "mes": "fevereiro", "lucro": 2000 }, { "mes": "março", "lucro": 2500 }, { "mes": "abril", "lucro": 1001 }, { "mes": "maio", "lucro": 0 }, { "mes": "junho", "lucro": -25 }]), [{ "mes": "fevereiro", "lucro": 2000 }, { "mes": "março", "lucro": 2500 }, { "mes": "abril", "lucro": 1001 }])
   
-  def test_afortunados_devuelve_una_lista_vacia_si_ningun_balance_tiene_ganancia_mayor_a_1000(self):
+  def test_afortunados_retorna_uma_lista_vazia_se_nenhum_balanço_tem_ganho_maior_que_1000(self):
     self.assertEqual(sortudo([{ "mes": "janeiro", "lucro": 1000 }, { "mes": "fevereiro", "lucro": 0 }, { "mes": "março", "lucro": 200 }, { "mes": "abril", "lucro": -30 }]), [])
