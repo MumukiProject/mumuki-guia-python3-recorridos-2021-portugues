732b9cea9eb075fab1a5673e78c4c85be0ddccc5
rocio@mumuki.org <rocio@mumuki.org>
2022-09-28 00:02:26 +0000

    Mumuki Export on 2022-09-28 00:02:26 +0000

--- 00009_Filtrar, filtrar cada coisa no seu lugar/test.py
+++ 00009_Filtrar, filtrar cada coisa no seu lugar/test.py
@@ -1,12 +1,12 @@
 
-  def test_balances_positivos_devuelve_todos_los_balances_si_todos_tienen_ganancia_mayor_a_cero(self):
+  def test_balanços_positivos_retorna_todos_os_balanços_se_todos_tem_ganho_maior_que_zero(self):
 	  self.assertEqual(balanco_positivo([{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro": 8 }]), [{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro": 8 }])
 
-  def test_balances_positivos_excluye_a_los_balances_con_ganancia_cero(self):
+  def test_balanços_positivos_exclui_os_balanços_com_ganho_zero(self):
 	  self.assertEqual(balanco_positivo([{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }, { "mes": "setembro", "lucro": 0 }]), [{ "mes": "março", "lucro": 10 }, { "mes": "agosto", "lucro": 2 }])
 
-  def test_balances_positivos_excluye_a_los_balances_con_ganancia_negativa(self):
+  def test_balanços_positivos_exclui_os_balanços_com_ganho_negativo(self):
 	  self.assertEqual(balanco_positivo([{ "mes": "julho", "lucro": 12 }, { "mes": "agosto", "lucro": -2 }]), [{ "mes": "julho", "lucro": 12 }])
  
-  def test_balances_positivos_devuelve_la_lista_vacia_si_todos_los_balances_tienen_ganancia_de_cero_o_menos(self):
+  def test_balanços_positivos_retorna_a_lista_vazia_se_todos_os_balanços_têm_ganho_zero_ou_menos(self):
 	  self.assertEqual(balanco_positivo([{ "mes": "agosto", "lucro": -12 }, { "mes": "setembro", "lucro": 0 }]), [])
