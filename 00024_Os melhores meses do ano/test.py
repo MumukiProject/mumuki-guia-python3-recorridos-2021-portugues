3f07d1876809610f6712c582410563d984c13744
rocio@mumuki.org <rocio@mumuki.org>
2022-09-28 00:15:59 +0000

    Mumuki Export on 2022-09-28 00:15:59 +0000

--- 00024_Os melhores meses do ano/test.py
+++ 00024_Os melhores meses do ano/test.py
@@ -2,5 +2,5 @@
   def test_meses_sortudos_retorna_os_meses_dos_balanços_sortudos(self):
     self.assertEqual(meses_sortudos([{ "mes": "janeiro", "lucro": 1001 }, { "mes": "fevereiro", "lucro": -10 }, { "mes": "março", "lucro": 2300 }, { "mes": "abril", "lucro": 800 }]), ["janeiro", "março"])
     
-  def test_meses_sortudos_retorna_uma_lista_vazia_se_não_houve_balanços_afortunados(self):
+  def test_meses_sortudos_retorna_uma_lista_vazia_se_não_houve_balanços_sortudos(self):
     self.assertEqual(meses_sortudos([{ "mes": "janeiro", "lucro": 999 }, { "mes": "fevereiro", "lucro": -10 }, { "mes": "março", "lucro": 20 }, { "mes": "abril", "lucro": 800 }]), [])
